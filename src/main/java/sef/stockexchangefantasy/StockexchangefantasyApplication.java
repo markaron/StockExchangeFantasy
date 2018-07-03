42a88317ef6a98c6f9d941c5ee71d51d036f7df6
Mark Kovacs <drmarkaron@gmail.com>
2018-07-03 12:22:33 +0200

    init commit

---
+++ src/main/java/sef/stockexchangefantasy/StockexchangefantasyApplication.java
@@ -0,0 +1,12 @@
+package sef.stockexchangefantasy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StockexchangefantasyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StockexchangefantasyApplication.class, args);
+	}
+}
